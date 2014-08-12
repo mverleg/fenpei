468c5763c9a63529307f81b4410ff1356cd6ad08
mulan <mark.verleg@gmail.com>
2014-08-12 16:32:22 +0800

    job_sh_single default subs

--- job_sh_single.py
+++ job_sh_single.py
@@ -4,7 +4,9 @@
 
 	automatically adds all substitutions as attributes to the job
 """
+#todo: could possibly be more efficient by not converting to ShJob
 
+from copy import copy
 from fenpei.job_sh import ShJob
 
 
@@ -18,11 +20,30 @@ class ShJobSingle(ShJob):
 			:param files: files to which substitutions should be applied
 			:param nosub_files: files as-is (no substitutions)
 		"""
+		""" defaults for substitutions """
+		subs_with_defaults = copy(self.get_default_subs())
+		subs_with_defaults.update(subs)
+		""" substitutions as job properties """
 		for key, val in subs.items():
 			setattr(self, key, val)
-		substitutions = {filepath: subs for filepath in self.get_sub_files() + sub_files}.update({
-						 filepath: None for filepath in self.get_nosub_files() + nosub_files})
+		""" convert to per-file format to make a ShJob """
+		substitutions = {filepath: subs_with_defaults for filepath in self.get_sub_files() + sub_files}
+		substitutions.update({filepath: None for filepath in self.get_nosub_files() + nosub_files})
 		super(ShJobSingle, self).__init__(name = name, substitutions = substitutions, weight = weight, batch_name = batch_name)
+
+	@classmethod
+	def get_default_subs(cls):
+		"""
+			:return: default values for substitutions
+		"""
+		return {}
+
+	@classmethod
+	def get_files(cls):
+		"""
+			(used by ShJob; make sure jobs are nod added twice)
+		"""
+		return []
 
 	@classmethod
 	def get_sub_files(cls):
