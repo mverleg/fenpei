5e7c481d9597023816386eb32c751daadb97a6cf
rafiki <mark@rafiki>
2015-12-15 14:06:20 +0100

    reorganize to make installable with pip & add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+	name='fenpei',  # should have used a - sorry
+	description='task distribution',
+	long_description='',
+	url='',
+	author='Mark V',
+	maintainer='(the author)',
+	author_email='mdilligaf@gmail.com',
+	license='Revised BSD License (LICENSE.txt)',
+	keywords=[],
+	version='1.0',
+	packages=['fenpei'],
+	include_package_data=True,
+	zip_safe=False,
+	classifiers=[
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'License :: OSI Approved :: BSD License',
+	],
+	install_requires=[
+	],
+)
+
+
