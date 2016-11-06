de5b70429a30c96c916fac52f450945543cf9419
rafiki <mark@rafiki>
2016-11-06 17:25:19 +0100

    job that combines subjobs (works, but is it useful enough?)

--- fenpei/combi_sh_single.py
+++ fenpei/combi_sh_single.py
@@ -10,27 +10,36 @@ from fenpei.job_sh_single import ShJobSingle
 
 class CombiSingle(ShJobSingle):
 	#todo: don't actually need the 'Sh'(shell) part, but that's how the hierarchy grew
-
-	def __init__(self, name, subs, ranges, child_cls, batch_name=None, child_kwargs=None, **kwargs):
+	#todo: this is kind of a hybrid between a queue and a job, which is undesirable design
+	
+	def __init__(self, name, subs, ranges, child_cls, batch_name=None, child_kwargs=None,
+			name_template=None, aggregation_func=None, **kwargs):
 		child_kwargs = child_kwargs or {}
 		self._child_cls = child_cls
 		self._child_jobs = []
-		self.weight = 0
+		weight = 0
 		params = ranges.keys()
 		combis = product(*ranges.values())
 		for combi in combis:
 			childsubs = copy(subs)
-			nameparts = []
 			for param, value in zip(params, combi):
 				childsubs[param] = value
-				nameparts.append('{1:s}{2:s}'.format(name, param, '{0:02d}'.format(value) if isinstance(value, int) else value))
-			childname = '_'.join([name, ''] + sorted(nameparts))
+			if name_template is None:
+				nameparts = []
+				for param, value in zip(params, combi):
+					nameparts.append('{1:s}{2:}'.format(name, param, value))
+				childname = '_'.join([name, ''] + sorted(nameparts))
+			else:
+				childname = name_template.format(name=name, **childsubs)
 			subjob = child_cls(name=childname, subs=childsubs, batch_name=batch_name, **child_kwargs)
 			self._child_jobs.append(subjob)
-			self.weight += subjob.weight
+			weight += subjob.weight
 		sub_range = copy(subs)
 		sub_range.update(ranges)
-		super(CombiSingle, self).__init__(name=name, subs=sub_range, batch_name=batch_name, weight=1, **kwargs)
+		super(CombiSingle, self).__init__(name=name, subs=sub_range, batch_name=batch_name, weight=weight, **kwargs)
+		if aggregation_func is None:
+			aggregation_func = self.aggregate
+		self.aggregation_func = aggregation_func
 	
 	def __repr__(self):
 		return '{0:s}*{1:d}'.format(super(CombiSingle, self).__repr__(), len(self._child_jobs))
@@ -65,9 +74,9 @@ class CombiSingle(ShJobSingle):
 		self._queue_children()
 		for job in self._child_jobs:
 			if not job.is_prepared():
-				self._log('{0:s} not prepared because {1:s} is not'.format(self, job), level=2)
+				self._log('{0:s} not prepared because {1:s} is not'.format(self, job), level=3)
 				return False
-		self._log('{0:s} prepared because all {1:d} children are'.format(self, len(self._child_jobs)), level=2)
+		self._log('{0:s} prepared because all {1:d} children are'.format(self, len(self._child_jobs)), level=3)
 		return True
 
 	def is_started(self):
@@ -75,9 +84,9 @@ class CombiSingle(ShJobSingle):
 		self._queue_children()
 		for job in self._child_jobs:
 			if not job.is_started():
-				self._log('{0:s} not started because {1:s} is not'.format(self, job), level=2)
+				self._log('{0:s} not started because {1:s} is not'.format(self, job), level=3)
 				return False
-		self._log('{0:s} started because all {1:d} children are'.format(self, len(self._child_jobs)), level=2)
+		self._log('{0:s} started because all {1:d} children are'.format(self, len(self._child_jobs)), level=3)
 		return True
 
 	def is_running(self):
@@ -85,18 +94,18 @@ class CombiSingle(ShJobSingle):
 		self._queue_children()
 		for job in self._child_jobs:
 			if job.is_running():
-				self._log('{0:s} running because {1:s} is'.format(self, job), level=2)
+				self._log('{0:s} running because {1:s} is'.format(self, job), level=3)
 				return True
-		self._log('{0:s} not running because not all {1:d} children are'.format(self, len(self._child_jobs)), level=2)
+		self._log('{0:s} not running because not all {1:d} children are'.format(self, len(self._child_jobs)), level=3)
 		return False
 
 	def is_complete(self):
 		self._queue_children()
 		for job in self._child_jobs:
 			if not job.is_complete():
-				self._log('{0:s} not complete because {1:s} is not'.format(self, job), level=2)
+				self._log('{0:s} not complete because {1:s} is not'.format(self, job), level=3)
 				return False
-		self._log('{0:s} complete because all {1:d} children are'.format(self, len(self._child_jobs)), level=2)
+		self._log('{0:s} complete because all {1:d} children are'.format(self, len(self._child_jobs)), level=3)
 		return True
 
 	def prepare(self, verbosity=0, *args, **kwargs):
@@ -135,10 +144,13 @@ class CombiSingle(ShJobSingle):
 		return bool(cnt)
 
 	def result(self, *args, **kwargs):
+		assert self.queue, 'cannot get results for {0:} since it doesn\'t have a queue'.format(self)
 		if not self.is_complete():
 			return None
-		return None
-		#todo: aggregate
+		job_results = self.queue.result(jobs=self._child_jobs)
+		if not job_results:
+			return None
+		return self.aggregation_func(job_results)
 
 	def _crash_reason_if_crashed(self, verbosity=0, *args, **kwargs):
 		completed, crashed = 0, 0
@@ -149,5 +161,11 @@ class CombiSingle(ShJobSingle):
 			if job._crash_reason_if_crashed():
 				crashed += 1
 		return '+' * completed + 'X' * crashed + '.' * (len(self._child_jobs) - completed - crashed)
+	
+	def aggregate(self, job_results):
+		"""
+		Combine results from child jobs. Used if `aggregation_func` argument is not provided.
+		"""
+		return {res['name']: res for res in job_results.values()}
 
 
